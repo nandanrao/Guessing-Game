ea99440f0ffba1957bcab318e5fc703f022f4cd0
Nandan Rao <nandanmarkrao@gmail.com>
2014-09-04 01:13:02 -0700

    made it fully gradiated

--- js/main.js
+++ js/main.js
@@ -20,47 +20,26 @@ function sortGuesses(arr) {
     });
 };
 
-function hotToWarm(container) {
-    var number = $(container).children().length;
-    $(container).find('span').each(function(i) {
-        var self = $(this),
-        r = Math.round(255),
-        g = Math.round(30 + i*120/number),
-        b = Math.round(0),
-        a = (1); 
+function warmUp(el, colorAmounts){        
+    var self = el;
+    r = Math.round(255),
+    g = Math.round(30 + 225*colorAmounts),
+    b = Math.round(0),
+    a = 1; 
     self.css({'color': 'rgba('+ r +','+ g +','+ b +','+ a +')'});
-    }); 
-}
-
-
-function warmingLower(container) {
-    var number = $(container).children().length;
-    $(container).find('span').each(function(i) {
-        var self = $(this),
-        r = Math.round(255),
-        g = Math.round(150 + i*80/number),
-        b = Math.round(0),
-        a = (1); 
+}; 
+    
+function coolDown(el, colorAmounts){        
+    var self = el;
+    r = Math.round(0),
+    g = Math.round(215 - 215*colorAmounts),
+    b = Math.round(255),
+    a = 1; 
     self.css({'color': 'rgba('+ r +','+ g +','+ b +','+ a +')'});
-    }); 
-}
-
-
-function warmToCold(container) {
-    var number = $(container).children().length;
-    $(container).find('span').each(function(i) {
-        var self = $(this),
-        r = Math.round(0),
-        g = Math.round(235 - i*120/number),
-        b = Math.round(255),
-        a = (1); 
-    self.css({'color': 'rgba('+ r +','+ g +','+ b +','+ a +')'});
-    }); 
-}
+}; 
 
 // Generate our magic number!
 generateDaNumber();
-
 
 // Just showing that number to the console for debugging
 console.log(danumber);
@@ -108,42 +87,49 @@ $("form").submit(function(event){
         direction = 'low';
     }
     
-    // Create object with information about this guess
-    var this_guess = {
-        num: daguess,
-        // Determine how warm/hot a guess is    
-        amt: Math.abs(danumber - daguess), 
-        dir: direction,
-    };
-    
-    // Determine if new guess is hotter or colder than previous guess
+    // Determine if new guess is hotter or colder than previous guess (not currently used)
     var last_guess = guesses[guesses.length-1];
     var hotter;
     
     if (guesses.length > 0){
-        if (this_guess.amt < last_guess.amt) {
+        if (amount < last_guess.amt) {
             hotter = true;
         }
         else {
             hotter = false;
         }
-    }
+    }   
     
-    // Print out hotter/colder (not currently being used)...
-    if (guesses.length > 0){
-        if (!hotter) {
-            console.log("colder!");
+    // See how far off the guess is
+    var amount = Math.abs(danumber - daguess);
+    
+    // set color for guess
+    var color;
+    if (amount < 25){
+        color = amount/25;
+    }
+    else {
+        if (direction == 'high'){
+            color = (daguess-danumber-25)/(100-danumber-25)
         }
         else {
-            console.log("hotter!");
+             color = (danumber-daguess-25)/(danumber-25)
         }
-    }    
+    }
+
+    // Create object with information about this guess
+    var this_guess = {
+        num: daguess,
+        amt: amount, 
+        dir: direction,
+        clr: color,
+    };
     
-    // Store all guess chronologically
+    // Store all guesses chronologically
     guesses.push(this_guess);
     
     // Store guesses into high and low arrays
-    if (this_guess.dir === 'high' && !again) {
+    if (direction === 'high' && !again) {
         higher_guesses.push(this_guess);
     }
     else if (!again) {
@@ -157,47 +143,33 @@ $("form").submit(function(event){
     // Reverse high guesses for color gradients
     higher_guesses.reverse();
     
-    
     // Empty text field after guess submitted
     $("input#guess").val('');
     
     // Empty out guess divs with every submit
     $("div.guesses > div").empty();
-    
+
     // Sort & display guesses!
     for (var i=higher_guesses.length - 1; i >= 0; i--) {
-        if (higher_guesses[i].amt > 25) {
-            $("div#higher .cold").append("<span>" + higher_guesses[i].num + "</span>");
-        }
-        else if (higher_guesses[i].amt > 10) {
-            $("div#higher .warm").append("<span>" + higher_guesses[i].num + "</span>");
+        $("div#higher .hot").append("<span id='" + higher_guesses[i].num + "'>" + higher_guesses[i].num + "</span>");
+        if (higher_guesses[i].amt < 25) {
+            warmUp($('span#' + higher_guesses[i].num), higher_guesses[i].clr);
         }
         else {
-            $("div#higher .hot").append("<span>" + higher_guesses[i].num + "</span>");    
+            coolDown($('span#' + higher_guesses[i].num), higher_guesses[i].clr);
         }
     }
     
     for (var i=lower_guesses.length - 1; i >= 0; i--) {
-         if (lower_guesses[i].amt > 25) {
-            $("div#lower .cold").append("<span>" + lower_guesses[i].num + "</span>");
-         }
-        else if (lower_guesses[i].amt > 10) {
-              $("div#lower .warm").append("<span>" + lower_guesses[i].num + "</span>");  
+        $("div#lower .hot").append("<span id='" + lower_guesses[i].num + "'>" + lower_guesses[i].num + "</span>");
+        if (lower_guesses[i].amt < 25) {
+            warmUp($('span#' + lower_guesses[i].num), lower_guesses[i].clr);
         }
         else {
-              $("div#lower .hot").append("<span>" + lower_guesses[i].num + "</span>");
+            coolDown($('span#' + lower_guesses[i].num), lower_guesses[i].clr);
         }
+         
     }
-
-    // Apply color to the past guesses
-    hotToWarm($('div#higher .hot'));
-    hotToWarm($('div#lower .hot')); 
-    
-    warmingLower($('div#higher .warm'));
-    warmingLower($('div#lower .warm'));
-    
-    warmToCold($('div#higher .cold'));
-    warmToCold($('div#lower .cold'));
     
     // End game if user guesses too many times 
     if (guesses.length > 20) {
